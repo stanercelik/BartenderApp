1b9582c5625a11710fea5f9230df987089f7312c
stanercelik <tanercelik2001@gmail.com>
2024-05-13 19:07:14 +0300

    Created Base Network and Detail Screen

---
+++ BartenderApp/Model/CocktailDetailModel.swift
@@ -0,0 +1,8 @@
+//
+//  cocktailDetailModel.swift
+//  BartenderApp
+//
+//  Created by Taner Çelik on 13.05.2024.
+//
+
+import Foundation
---
+++ BartenderApp/Service/BaseNetwork.swift
@@ -0,0 +1,8 @@
+//
+//  BaseNetwork.swift
+//  BartenderApp
+//
+//  Created by Taner Çelik on 13.05.2024.
+//
+
+import Foundation
